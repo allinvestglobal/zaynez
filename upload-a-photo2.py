50911963d8b47e16f1c141c17da82c1351d3fa69
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-09-10 11:11:27 +0200

    Add files via upload

---
+++ upload-a-photo2.py
@@ -0,0 +1,549 @@
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'montalban4.jpeg'
+text = 'The best stories can be found between the pages of a passport'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'toledo3.jpeg'
+text = 'Some days I go to see the world but most days I try to change it'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'valencia3.jpeg'
+text = 'Thoughts create things'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'toledo2.jpeg'
+text = 'I still remember the days I prayed for the things I have now'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'wine.jpeg'
+text = 'All I been doing is celebrating, dont even now what Im celebrating'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'valencia2.jpeg'
+text = 'It looks like Rome but its Valencia'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'montalban3.jpeg'
+text = 'I always knew I would be King haha!'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'bridge.jpeg'
+text = 'Valencia, Spain reminds me of Rotterdam, not because of look but because of the creativity'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'moltaban2.jpeg'
+text = 'I knew I could never be a singer so I became a businessman so atleast I can dress like one'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'park.jpeg'
+text = 'I want to change the world but I dont think most people think like me'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'black.jpeg'
+text = 'Some people dont notice me, but some look at me like they know all my stories'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'jimrohn.jpeg'
+text = 'Walk away from the 97% who live in the past and join the 3% who create the future'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'museum2.jpeg'
+text = 'Where could your dreams take you'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'gold.jpeg'
+text = 'When you change the way you look at things, the things you look at change Wayne Dyer'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'wedding.jpeg'
+text = 'When I was about 8, I told my mother I would make her rich one day.. and I never forgot'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'talavera.jpeg'
+text = 'I took the wrong bus and came to Talavera.. but now I think it was the right bus'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'graf.jpeg'
+text = 'The kids are so talented, but most dont believe in their greatness'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'chucky.jpeg'
+text = 'Chucky wants to play haha!'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'museum.jpeg'
+text = 'The history of the world is so curious'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'toledo.jpeg'
+text = 'The King of the past and the king of the future'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'beach.jpeg'
+text = 'In Barcelona, there is a street called La Rambla with hundreds of people... I could live on that street'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'belive.jpeg'
+text = 'This is my favorite place in Talavera, the energy here is amazing'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'giants.jpeg'
+text = 'If I have seen furthur, it was because I stood on the soilders of Giants'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'madrid2.jpeg'
+text = 'Better 30 years full of adventure, than 100 years safe in the corner Jim Rohn'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'mol.jpeg'
+text = 'I always knew I would be rich, I never doubted it for a second Warren Buffet'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'moltalban.jpeg'
+text = 'Happiness comes from achievement Tony Robbins'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'red.jpeg'
+text = 'She asked, what have I learned from getting richer, I learn working with the negatives could make for better pictures'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'rotterdam.jpeg'
+text = 'If you start young, the fortune belongs to you Jim Rohn'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'ukoffice.jpeg'
+text = 'Reality is wrong, dreams are for real Tupac'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'luciaoffice.jpeg'
+text = 'My first office.. who knew life would move so fast'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'valencia.jpeg'
+text = 'What will the legacy of your life be?'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'madrid.jpeg'
+text = 'It is not that I am lucky, I just started really early..10 years ago early'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'tbabe.jpeg'
+text = 'I love how God gives you everything you need, you just have to work with the good and the bad, its all required'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'cool.jpeg'
+text = 'Those who think it cant be done shouldnt interupt those who are doing it'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'car.jpeg'
+text = 'You dont get paid by the hour, you get paid by the value you bring to the hour Jim Rohn'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'sl.jpeg'
+text = 'The expert has failed more times than the beginner has even tried'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'suit.jpeg'
+text = 'How do you achieve greatness? Find a way to serve many people Jim Rohn'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'xmus.jpeg'
+text = 'She asked why red? I replied because I already wore black yesterday'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedemadrid'
+password = '20million'
+image = 'valencia4.jpeg'
+text = 'My secrect is massive action, hundreds of audiobooks and years of learning, the only way to do it is to do it'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
